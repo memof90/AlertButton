afd193658f220bf4d8e02bc614b1e64701fded42
Memo Figueredo <memofigueredo@macbook-pro-de-memo.local>
2018-12-20 15:01:23 -0500

    Initial Commit

---
+++ AlertButton/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  AlertButton
+//
+//  Created by Memo Figueredo on 12/20/18.
+//  Copyright © 2018 Memo Figueredo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, Word", message: "This is my first app", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: " Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+}
+
